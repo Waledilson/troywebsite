865f97cd32468ce1cd8e3f63fd9bcc0855f30308
Waledilson <95579186+Waledilson@users.noreply.github.com>
2024-03-09 20:52:14 -0400

    added surge logo, but its all fucked up

--- src/components/basement-card/basement-card.jsx
+++ src/components/basement-card/basement-card.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Card from "react-bootstrap/Card"
 import CardFooter from "react-bootstrap/CardFooter"
+import Surgelogo from "../../../src/img/surge-logo.webp"
+
 import Aos from "aos"
 import "aos/dist/aos.css"
 import { useEffect } from "react"
@@ -15,28 +17,31 @@ function BasementSessionsCard() {
       className="event-card"
       style={{ width: "90vw" }}
     >
-      <Card.Body>
-        <Card.Header as="h1" className="event-card-title">
-          Surge Radio & Arseneault Group Entertainment Presents:
-          <Card.Title as="h1" className="mb-2 text-muted">
-            The Basement Sessions
-          </Card.Title>
-        </Card.Header>
-        {/* <Card.Subtitle as="h2" className="mb-2 text-muted">
+      <Card.Img src={Surgelogo} className="surgelogo" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Body>
+          <Card.Header as="h1" className="event-card-title">
+            Surge Radio & Arseneault Group Entertainment Presents:
+            <Card.Title as="h1" className="mb-2 text-muted">
+              The Basement Sessions
+            </Card.Title>
+          </Card.Header>
+          {/* <Card.Subtitle as="h2" className="mb-2 text-muted">
             The Basement Sessions
           </Card.Subtitle> */}
-        <Card.Text as="p" className="event-card-body">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Text>Time & Place: 8:00PM / The Basement</Card.Text>
-        <Card.Text>Cost: $12,000</Card.Text>
+          <Card.Text as="p" className="event-card-body">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Text>Time & Place: 8:00PM / The Basement</Card.Text>
+          <Card.Text>Cost: $12,000</Card.Text>
 
-        <CardFooter className="event-card-links">
-          {/* <Card.Link href="#">Event Page</Card.Link> */}
-          <Card.Link href="#">Tickets</Card.Link>
-        </CardFooter>
-      </Card.Body>
+          <CardFooter className="event-card-links">
+            {/* <Card.Link href="#">Event Page</Card.Link> */}
+            <Card.Link href="#">Tickets</Card.Link>
+          </CardFooter>
+        </Card.Body>
+      </Card.ImgOverlay>
     </Card>
   )
 }
