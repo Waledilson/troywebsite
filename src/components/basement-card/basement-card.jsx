1e9251aad60abb8943201372877dbe4709b9e5fc
Waledilson <95579186+Waledilson@users.noreply.github.com>
2024-04-09 23:54:37 -0300

    added event images

--- src/components/basement-card/basement-card.jsx
+++ src/components/basement-card/basement-card.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import Card from "react-bootstrap/Card"
 import CardFooter from "react-bootstrap/CardFooter"
 import Surgelogo from "../../../src/img/surge-logo.webp"
+import BasementImg from "../../../src/img/TheBasement.png"
+import Button from "react-bootstrap/Button"
 
 import Aos from "aos"
 import "aos/dist/aos.css"
@@ -20,26 +22,27 @@ function BasementSessionsCard() {
       {/* <Card.Img src={Surgelogo} className="surgelogo" alt="Card image" /> */}
       {/* <Card.ImgOverlay> */}
       <Card.Body className="basement-card">
-        <Card.Header as="h1" className="event-card-title">
+        <Card.Img className="basement-img" variant="top" src={BasementImg} />
+
+        {/* <Card.Header as="h1" className="event-card-title">
           <img src={Surgelogo} height="130px" witdth="160px" /> & Arseneault
           Group Entertainment Presents:
           <Card.Title as="h1" className="mb-2">
             The Basement Sessions
           </Card.Title>
         </Card.Header>
-        {/* <Card.Subtitle as="h2" className="mb-2 text-muted">
-            The Basement Sessions
-          </Card.Subtitle> */}
+        
         <Card.Text as="p" className="event-card-body">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
         <Card.Text>Time & Place: 8:00PM / The Basement</Card.Text>
-        <Card.Text>Cost: $12,000</Card.Text>
+        <Card.Text>Cost: $12,000</Card.Text> */}
 
         <CardFooter className="event-card-links">
-          {/* <Card.Link href="#">Event Page</Card.Link> */}
-          <Card.Link href="#">Tickets</Card.Link>
+          {/* <Button className="ticket-button" size="lg" variant="secondary">
+            TICKETS
+          </Button> */}
         </CardFooter>
       </Card.Body>
       {/* </Card.ImgOverlay> */}
