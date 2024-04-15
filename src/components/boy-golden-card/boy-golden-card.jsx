2ee90fa5c46884615e5d2ddae0eacfcfc7a28601
Waledilson <95579186+Waledilson@users.noreply.github.com>
2024-04-15 11:03:18 -0300

    added show details to boygolden card

--- src/components/boy-golden-card/boy-golden-card.jsx
+++ src/components/boy-golden-card/boy-golden-card.jsx
@@ -22,18 +22,54 @@ function BoyGoldenCard() {
         <Card.Img className="boy-golden-img" variant="top" src={BoyGoldenImg} />
         {/* <Card.Header as="h1" className="event-card-title">
           <img src={Surgelogo} height="130px" witdth="160px" /> & Arseneault
-          Group Entertainment Presents:
-          <Card.Title as="h1" className="mb-2 ">
-            Boy Golden
-          </Card.Title>
-        </Card.Header>
+          Group Entertainment Presents: */}
+        {/* <Card.Title as="h1" className="mb-2">
+          Boy Golden
+        </Card.Title> */}
+        {/* </Card.Header> */}
         <Card.Text as="p" className="event-card-body">
-          When listening to his music, it feels easy to dissolve into the ether.
-          Everything flows. From classic country to psych-folk, Alternative to
-          roadhouse pop to Appalaichan bluegrass, Boy Golden’s music is easy,
-          breezy, warm and gritty. And don’t it just feel good to listen to it.
+          <h1>Boy Golden</h1>
+          <p>
+            Boy Golden Everything becomes fluid when you can pass through time
+            and space like a ghost, a story, a melody. Boy Golden manifests all
+            three on For Jimmy. When listening to his music, it feels easy to
+            dissolve into the ether. Everything flows. Introspective and
+            vulnerable, traditional and queer, hard-headed and sensual, Boy
+            Golden&apos;s everyman-aesthetic appeals to us all. No matter what
+            frame of mind he&apos;s in, he&apos;ll fit us in for a good time.
+            We&apos;re here right now. Since releasing his debut album, Church
+            of Better Daze, in 2021, he&apos;s played every summer festival on
+            your list, curated and directed an art show and music video for “KD
+            & Lunchmeat”, the Seth-Rogenesque hit single that charted to #1 on
+            Alternative Radio, and toured with The Sheepdogs on a 60 date North
+            American tour.
+          </p>
+          <Button
+            className="artist-website-button"
+            href="https://www.boygolden.ca/"
+            variant="secondary"
+          >
+            Boy Golden Website
+          </Button>
+          <h1>Fontine</h1>
+          <p>
+            A hush sweeps across Turtle Island. Birds flock to perch on towering
+            maples and the waving prairie grasses still. There is a name upon
+            the tongue of every creature from the elk to the shrew. FONTINE. A
+            gust of wind blows open the shutters of every home and young and old
+            kneel in reverence to the dawn of a new era. FONTINE. Queen of the
+            road, King of the song, FONTINE is here with a brand new EP.
+          </p>
+          <Button
+            className="artist-website-button"
+            href="https://fontine.bandcamp.com/"
+            variant="secondary"
+          >
+            Fontine Bandcamp
+          </Button>
         </Card.Text>
-        <Card.Text>Time & Place: May 17th 2024 / The Seahorse</Card.Text>
+
+        {/* <Card.Text>Time & Place: May 17th 2024 / The Seahorse</Card.Text>
         <Card.Text>Cost: $12,000</Card.Text> */}
         {/* <img
           src={BoyGoldenImg}
